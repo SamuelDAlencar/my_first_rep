d42e1651caa799843a1dc2d4d1a6928edcfda6d4
SamuelDAlencar <sarl.2001@hotmail.com>
2021-10-11 21:46:21 -0300

    exercises addition

--- bloco_4-introduçao_a_javascript_e_logica_de_programaçao/day_1/javascript_arithmetic_exercises.js
+++ bloco_4-introduçao_a_javascript_e_logica_de_programaçao/day_1/javascript_arithmetic_exercises.js
@@ -1,6 +1,6 @@
-let a = 7
-let b = 18
-let c = 4
+let a = 7;
+let b = 18;
+let c = 4;
 
 console.log(a + b)
 console.log(a - b)
@@ -24,7 +24,7 @@ if (a > b && c) {
 
 // ----------------------------------
 
-const d = 35
+const d = 35;
 
 if (d > 0) {
   console.log("positive")
@@ -36,9 +36,9 @@ if (d > 0) {
 
 // ----------------------------------
 
-const L1 = 60
-const L2 = 60
-const L3 = 60
+const L1 = 60;
+const L2 = 60;
+const L3 = 60;
 
 if (L1 + L2 + L3 == 180) {
   console.log("true")
@@ -46,4 +46,65 @@ if (L1 + L2 + L3 == 180) {
   console.log("error")
 }
 
+// ----------------------------------
 
+let piece = "bishop"
+
+piece.toUpperCase();
+
+switch (piece) {
+  case "king": console.log("Any direction")
+    break;
+
+  case "bishop": console.log("Diagonally")
+    break;
+  
+  case "knight": console.log("L shape")
+    break;
+}
+
+// ---------------------------------- 
+
+let nota = 90
+
+if (nota >= 90) {
+  console.log("A")
+} else if (nota >= 90) {
+  console.log("B")
+} else if (nota >= 80) {
+  console.log("C")
+} else if (nota >= 70) {
+  console.log("D")
+} else if (nota >= 60) {
+  console.log("E")
+} else if (nota < 50) {
+  console.log("F")
+} else if (nota < 0 || nota > 100) {
+  console.log ("!!! error !!!")
+}
+
+// ---------------------------------- 
+
+const number1 = 16;
+const number2 = 7;
+const number3 = 35;
+
+if (number1 % 2 == 0 || number2 % 2 == 0 || number3 % 2 == 0) {
+  console.log(true)
+} else {
+  console.log(false)
+}
+
+// ---------------------------------- 
+
+const numberA = 54;
+const numberB = 17;
+const numberC = 26;
+
+if (numberA % 2 > 0 || numberB % 2 > 0 || numberC % 2 > 0) {
+  console.log(true)
+} else {
+  console.log(false)
+}
+
+// ---------------------------------- 
