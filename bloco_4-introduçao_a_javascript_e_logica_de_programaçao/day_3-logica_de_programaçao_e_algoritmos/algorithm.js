08f144097b2005336b118eafcf93941597d6f376
SamuelDAlencar <sarl.2001@hotmail.com>
2021-10-14 22:34:33 -0300

    2 'for' exercises resolution

---
+++ bloco_4-introduçao_a_javascript_e_logica_de_programaçao/day_3-logica_de_programaçao_e_algoritmos/algorithm.js
@@ -0,0 +1,35 @@
+// Exercise 1
+
+let n = 4;
+let text1 = "";
+
+for (let iC = 0 ; iC < n ; iC++) {
+  text1 += "*"
+}
+
+for (iL = 0; iL < n ; iL++) {
+  console.log(text1)
+}
+
+console.log("---------------------------")
+
+// ------------------------------------------
+// Exercise 2
+
+let text2 = "";
+
+for (let i = 0 ; i < n ; i++) {
+  text2 += "*"
+
+  console.log(text2)
+}
+
+console.log("---------------------------")
+
+// ------------------------------------------
+// Exercise 3
+
+
+
+
+
