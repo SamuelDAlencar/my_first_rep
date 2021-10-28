9119d8cada9004c1453983d89a9b6b4df17c0fcd
SamuelDAlencar <sarl.2001@hotmail.com>
2021-10-28 19:49:59 -0300

    form exercise

---
+++ modulo_1-fundamentos_do_desenvolvimento_web/bloco_6-html_e_css:forms,_flexbox_e_responsivo/html_and_css-forms/script.js
@@ -0,0 +1,12 @@
+
+function elementCreator (howMany, parentId, childElement) {
+  stateArray = ['Minas Gerais', 'São Paulo', 'Rio Grande Do Sul', 'Amapá']
+
+  for (let i = 0; i < howMany; i += 1) {
+    let optionCreator = document.getElementById(parentId).appendChild(document.createElement(childElement))
+
+    document.getElementById(parentId).children[i].innerHTML = stateArray[i]
+  }
+}
+
+elementCreator(4, 'estado', 'option')
