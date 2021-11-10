9b01ca41e553d4597c54e7cc311f320d079eb354
SamuelDAlencar <sarl.2001@hotmail.com>
2021-11-10 16:25:54 -0300

    es6 exercise attempts

---
+++ modulo_1-fundamentos_do_desenvolvimento_web/bloco_7-introducao_a_javascript_es6_e_testes_unitarios/js_es6-let,_const,_arrow_functions_e_template_literals/script.js
@@ -0,0 +1,21 @@
+const testingScope = (escopo) => {
+  if (escopo === true) {
+    var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+    ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
+    console.log(ifScope);
+  } else {
+    var elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+    console.log(elseScope);
+  }
+  // console.log(`${ifScope} o que estou fazendo aqui ? :O`); // Se necessário esta linha pode ser removida.
+}
+
+testingScope(true);
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+const oddsAndEvensFunction = () => {
+  return oddsAndEvens.sort();
+}
+
+console.log(`Os números ${oddsAndEvensFunction()} se encontram ordenados de forma crescente!`); // será necessário alterar essa linha 😉
