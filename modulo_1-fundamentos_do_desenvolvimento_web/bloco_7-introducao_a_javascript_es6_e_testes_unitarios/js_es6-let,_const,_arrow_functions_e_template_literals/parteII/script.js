f0bf05e4cab6e13ea9d9df2c594aac6080a41915
SamuelDAlencar <sarl.2001@hotmail.com>
2021-11-11 13:11:14 -0300

    ES6 parteII exercise

--- modulo_1-fundamentos_do_desenvolvimento_web/bloco_7-introducao_a_javascript_es6_e_testes_unitarios/js_es6-let,_const,_arrow_functions_e_template_literals/parteII/script.js
+++ modulo_1-fundamentos_do_desenvolvimento_web/bloco_7-introducao_a_javascript_es6_e_testes_unitarios/js_es6-let,_const,_arrow_functions_e_template_literals/parteII/script.js
@@ -1,8 +1,25 @@
-const button = document.getElementById('counterButton');
-const howManyP = document.getElementById('howMany');
-let counter = 0;
+// const button = document.getElementById('counterButton');
+// const howManyP = document.getElementById('howMany');
+// let counter = 0;
 
-button.addEventListener('click', () => {
-  counter += 1
-  howManyP.innerText = counter
-});
+// button.addEventListener('click', () => {
+//   counter += 1
+//   howManyP.innerText = counter
+// });
+
+let phrase = 'Meu nome é Samuel x Alencar';
+let x = '';
+
+const xFinder = (string) => {
+  for (let i = 0; i < phrase.split(' ').length; i += 1) {
+    if (phrase.split(' ')[i] == 'x') {
+      x = phrase.split(' ')[i]
+
+      phrase = phrase.replace(x, string)
+    }
+  }
+
+  console.log(phrase)
+}
+
+xFinder('de')
