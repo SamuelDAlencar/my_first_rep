6faa5e0859824421e6fa51de1839ba67d6c2860a
SamuelDAlencar <sarl.2001@hotmail.com>
2021-11-13 22:40:19 -0300

    attemps in the students counter exercise

--- modulo_1-fundamentos_do_desenvolvimento_web/bloco_7-introducao_a_javascript_es6_e_testes_unitarios/js_es6-objects/partII/script.js
+++ modulo_1-fundamentos_do_desenvolvimento_web/bloco_7-introducao_a_javascript_es6_e_testes_unitarios/js_es6-objects/partII/script.js
@@ -27,4 +27,43 @@ const nightShift = (object, key, value) => {
 
 nightShift(lesson2, 'turno', 'noite')
 
-console.log(lesson2)
+const objectKeys = (object) => {
+  console.log(Object.keys(object))
+};
+
+objectKeys(lesson2);
+
+const howManyInObj = (object) => {
+  console.log(Object.keys(object).length)
+};
+
+howManyInObj(lesson2);
+
+const objectValues = (object) => {
+  console.log(Object.values(object))
+};
+
+objectValues(lesson2);
+
+let allLessons = {}
+
+const objectAdder = () => {
+  allLessons.lesson1 = Object.assign(lesson1)
+  allLessons.lesson2 = Object.assign(lesson2)
+  allLessons.lesson3 = Object.assign(lesson3)
+  console.log(allLessons);
+}
+
+objectAdder();
+
+let studentsCounter = 0;
+
+const howManyStudents = () => {
+  for (let i = 0; i < Object.keys(allLessons).length; i += 1) {
+    for (let c = 0; i < Object.values(allLessons)[i].length; c += 1) {
+
+    }
+  }
+}
+
+howManyStudents();
