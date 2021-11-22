4099251411835a83405114a2888180b900d22537
SamuelDAlencar <sarl.2001@hotmail.com>
2021-11-22 18:40:04 -0300

    reduce exercises

--- modulo_1-fundamentos_do_desenvolvimento_web/bloco_8-higher_higher_order_functions_do_javascript_es6/reduce/script.js
+++ modulo_1-fundamentos_do_desenvolvimento_web/bloco_8-higher_higher_order_functions_do_javascript_es6/reduce/script.js
@@ -7,14 +7,102 @@ const arrays = [
 ];
 
 function flatten() {
-  const arraysSum = arrays.reduce((array = [], currentItem) => {
-    array.push(currentItem);
+  const arraysSum = arrays.reduce((array, currentItem, i) => {
+    currentItem.forEach((item) => {
+      array.push(item)
+    })
 
     return array
-  })
+  }, [])
 
   return arraysSum;
 };
 
 console.log(flatten());
-console.log(arrays[1]);
+
+const books = [
+  {
+    id: 1,
+    name: 'As Crônicas de Gelo e Fogo',
+    genre: 'Fantasia',
+    author: {
+      name: 'George R. R. Martin',
+      birthYear: 1948,
+    },
+    releaseYear: 1991,
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: {
+      name: 'J. R. R. Tolkien',
+      birthYear: 1892,
+    },
+    releaseYear: 1954,
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Isaac Asimov',
+      birthYear: 1920,
+    },
+    releaseYear: 1951,
+  },
+  {
+    id: 4,
+    name: 'Duna',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Frank Herbert',
+      birthYear: 1920,
+    },
+    releaseYear: 1965,
+  },
+  {
+    id: 5,
+    name: 'A Coisa',
+    genre: 'Terror',
+    author: {
+      name: 'Stephen King',
+      birthYear: 1947,
+    },
+    releaseYear: 1986,
+  },
+  {
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: {
+      name: 'H. P. Lovecraft',
+      birthYear: 1890,
+    },
+    releaseYear: 1928,
+  },
+];
+
+// Exercise 2
+
+function reduceNames() {
+  const allNames = books.reduce((allNamesString, currentBook) => {
+    return allNamesString += `${currentBook.author.name}, `
+  }, '');
+
+  return allNames
+}
+
+console.log(reduceNames())
+
+// Exercise 3
+
+function averageAge() {
+  const agesSum = books.reduce((allAges, currentBook) => {
+    return allAges += (currentBook.releaseYear - currentBook.author.birthYear)
+  }, 0)
+
+  return agesSum / books.length
+}
+
+console.log(averageAge())
