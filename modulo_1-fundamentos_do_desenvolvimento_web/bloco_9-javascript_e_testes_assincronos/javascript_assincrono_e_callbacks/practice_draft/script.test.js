daa89bf5ea5bfb1343fc702220f44ea04a672900
SamuelDAlencar <sarl.2001@hotmail.com>
2021-11-30 10:41:08 -0300

    general attempts on practice test

--- modulo_1-fundamentos_do_desenvolvimento_web/bloco_9-javascript_e_testes_assincronos/javascript_assincrono_e_callbacks/practice_draft/script.test.js
+++ modulo_1-fundamentos_do_desenvolvimento_web/bloco_9-javascript_e_testes_assincronos/javascript_assincrono_e_callbacks/practice_draft/script.test.js
@@ -1,9 +1,4 @@
-const asyncSum = (a, b, callback) => {
-  setTimeout(() => {
-    const result = a + b;
-    callback(result);
-  }, 500);
-};
+const asyncSum = require('./script');
 
 test('Testando asyncSum, soma 5 mais 10', (done) => {
   asyncSum(5, 10, (result) => {
